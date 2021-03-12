f1e2eddc802f7b0ac1fd4fa29451ba190a3f383f
Wednesday <yensonyu@gmail.com>
2021-03-12 00:27:36 -0800

    create search page

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 
-export function Input(props) {
+// export function Input(props) {
+//     return (
+//       <div className="form-group">
+//         <input className="form-control" {...props} placeholder="Book Name" />
+//       </div>
+//     );
+// }
+
+// export function FormBtn(props) {
+//     return (
+//       <button {...props} className="btn btn-success">
+//         Search
+//       </button>
+//     );
+// }
+
+function SearchForm(props) {
     return (
-      <div className="form-group">
-        <input className="form-control" {...props} />
+      <div class="input-group mt-2">
+        <input {...props} type="text" class="form-control" placeholder="Book Name" />
+        <div class="input-group-append">
+          <button {...props} class="btn btn-success" type="button">Search</button>
+        </div>
       </div>
     );
 }
 
-export function FormBtn(props) {
-    return (
-      <button {...props} className="btn btn-success">
-        Search
-      </button>
-    );
-}
+export default SearchForm;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,5 +1,30 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import {Container, Row, Col} from "../components/Grid";
+import SearchForm from "../components/SearchForm";
+import backgroundImage from "../images/bgImage.jpg";
 
+function About() {
+    return (
+      <div>
+        <Hero backgroundImage={backgroundImage}>
+          <h1>Personnel</h1>
+          <h2>Find your books here!</h2>
+        </Hero>
+        <Container style={{ marginTop: 30 }}>
+          <Row>
+            <Col size="md-12">
+              <h1>Search Books</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-12">
+              <SearchForm />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+  
+  export default About;
