e5df9df6c417a5e7a0dbc6202f03193337e31eef
Wednesday <yensonyu@gmail.com>
2021-03-13 21:16:51 -0800

    set up book model

---
+++ models/book.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  bookId: { type: String, require: true },
+  title: { type: String, require: true },
+  authors: [{ type: String, require: true}],
+  description: { type: String, require: true },
+  link: { type: String, require: true },
+  image: { type: String, require: true }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
